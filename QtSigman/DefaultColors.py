3af87a6f789522e7515a61621b01ff08537c472b
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2017-11-23 20:22:41 +0100

    Udeterministyczniono przypisywanie kolorów do danych
    Funkcja pythonowa hash nie jest deterministyczna (zwraca inny wynik za
    każdym uruchomieniem). Zamieniona na random.seed i random.randint.

--- QtSigman/DefaultColors.py
+++ QtSigman/DefaultColors.py
@@ -1,19 +1,21 @@
 """Ten moduł zawiera listę podstawowych powiązań różnych rodzajów danych z
 kolorami.
 """
+import random
 defaultColors={'ecg':'C0', 'bp':'C1', 
              'r':'r', 
              'sbp':'#886600', 'dbp':'#AA9900', 'dn':'y',
              'hr':'C2'}
 
 def generateColor(text):
-    """Generuje kolor w oparciu o hash tekstu."""
-    return hex(abs(hash('a')%0xFFFFFF))
+    """Deterministycznie generuje kolor dla danego tekstu."""
+    random.seed(text)
+    return hex(random.randint(0,0xFFFFFF))
 
 def getColor(text):
     """Zwraca kolor dla danego tekstu. Jeśli jest on w dict
     defaultColors, zwraca odpowiadającą mu wartość. Jeśli nie, to 
-    generuje kolor w oparciu o hash tekstu.
+    deterministycznie generuje kolor w oparciu o tekst.
     """
     if text in defaultColors:
         return defaultColors[text]
