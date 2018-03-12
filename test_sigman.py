4fef90538c0605938114bf5bb1b0664bde351c2d
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-03-12 14:23:06 +0100

    Initial pytest tests

--- test_sigman.py
+++ test_sigman.py
@@ -1,122 +1,58 @@
 #!/usr/bin/env python3
-# W tym skrypcie zademonstrowane są wszystkie główne metody biblioteki sigman
+# This script tests functions & methods of the `sigman` library
+# it can be run by simply using `pytest` in the `sigman-project` directory
+
+import os
+import pytest
 
 import sigman as sm
 from sigman import file_manager as fm
 from sigman import analyzer
 from sigman import visualizer as vis
-import os
 
 os.path.dirname(os.path.abspath(__file__))
 
-print(">Próba importu próbych .dat")
-bp_wave = fm.import_wave('example_data/BP.dat', 'bp')
-ecg_wave = fm.import_wave('example_data/EKG.dat', 'ecg')
-r_points = fm.import_points('example_data/R.dat', 'r') 
+@pytest.fixture
+def bp_wave():
+    return fm.import_wave('example_data/BP.dat', 'bp')
 
-print(">Próba połączenia danych w Composite_data")
-waves={'bp':bp_wave, 'ecg':ecg_wave}
-points={'r':r_points}
-complete_data = sm.Composite_data(waves=waves, points=points)
+@pytest.fixture
+def r_points():
+    return fm.import_points('example_data/R.dat', 'r')
 
-print(">Próba wizualizacji danych w całości")
-vis.visualize_composite_data(complete_data, title="Całość")
+@pytest.fixture
+def butterworth():
+    return analyzer.import_procedure("modify_filter_butterworth")
 
-print(">Próba wizualizacji wycinka danych")
-vis.visualize_composite_data(complete_data, begin_time=60, end_time=80, title="Wycinek dwudziestosekundowy")
+def test_import_wave_dat():
+    bp_wave = fm.import_wave('example_data/BP.dat', 'bp')
+    assert bp_wave.value_at(2) == 134.75
 
-print(">Próba wizualizacji wycinka z jednym przebiegiem przesuniętym w czasie")
-complete_data.waves['bp'].offset = -0.2
-vis.visualize_composite_data(complete_data, begin_time=60, end_time=80, title="Wycinek dwudziestosekundowy z offsetem -0.2s na BP")
-complete_data.waves['bp'].offset = 0
+def test_import_points_dat():
+    r_points = fm.import_points('example_data/R.dat', 'r')
+    assert r_points[2][0] == 2.4950000000000001
 
-print(">Próba importu zewnętrznej procedury filtrowania i przefiltrowania nią danych")
-butterworth = analyzer.import_procedure("modify_filter_butterworth")
-print("Procedure type:",butterworth.procedure_type)
-print("Description:",butterworth.description)
-print("Author:",butterworth.author)
+def test_wave_offset(bp_wave):
+    bp_wave.offset = -2
+    assert bp_wave.value_at(0) == 134.75
 
-arguments = butterworth.default_arguments
-arguments['N'] = 3
-arguments['Wn'] = 30
-modified_wave = analyzer.modify_wave(complete_data.waves['bp'], 60, 70, butterworth, arguments)
-complete_data.waves['bp'].replace_slice(60, 70, modified_wave)
-vis.visualize_composite_data(complete_data, begin_time=60, end_time=80, title="Wycinek dwudziestosekundowy po filtracji 30 Hz na zakresie <60s;70s>")
+def test_points_offset(r_points):
+    r_points.offset(-2)
+    assert r_points[2][0] == 0.4950000000000001
 
-print(">Próba usunięcia punktów na danym zakresie")
-complete_data.points['r'].delete_slice(65,75)
-vis.visualize_composite_data(complete_data, begin_time=60, end_time=80, title="Wycinek dwudziestosekundowy po usunięciu punktów")
+def test_procedure_import():
+    butterworth = analyzer.import_procedure("modify_filter_butterworth")
+    assert butterworth.author == 'kcybulski'
 
-print(">Próba zapisania composite_data")
-fm.save_composite_data("example_data/example_composite_data.pickle",complete_data)
+def test_deprecated_modify_procedure(bp_wave, butterworth):
+    arguments = butterworth.default_arguments
+    arguments['N'] = 3
+    arguments['Wn'] = 30
+    filtered_wave = analyzer.modify_wave(bp_wave, 60, 70, butterworth,
+                                         arguments)
+    assert 90.118839599334365 == filtered_wave.value_at(5)
 
-print(">Proba wczytania innego, bardzo chaotycznego sygnału EKG i przefiltrowania go a następnie pokazania tuż obok nieprzefiltrowanego")
-ecg_wave = fm.import_wave('example_data/EKG_messy.dat', 'ecg')
-arguments['N'] = 3
-arguments['Wn'] = 20
-modified_ecg = analyzer.modify_wave(ecg_wave, 0, ecg_wave.complete_length,
-                                    butterworth, arguments)
-complete_data = sm.Composite_data(waves={'ecg_messy':ecg_wave,'ecg_clean':modified_ecg})
-vis.visualize_composite_data(complete_data, begin_time=10,end_time=15,title="EKG wejściowe (mocno zaburzone) oraz przefiltrowane filtrem 20 Hz")
-
-print(">Próba ponownego wczytania wcześniej zapisanego composite_data")
-complete_data = fm.load_composite_data('example_data/example_composite_data.pickle')
-vis.visualize_composite_data(complete_data, title="Wczytany ponownie composite_data")
-os.remove('example_data/example_composite_data.pickle')
-
-print(">Próba odnalezienia r na odcinku <65s, 75s>")
-find_r = analyzer.import_procedure('points_r_simple')
-arguments = find_r.default_arguments
-found_points = analyzer.find_points(complete_data.waves, complete_data.points, 65, 75, find_r, arguments)
-complete_data.add_points(found_points, 'r', join=True)
-vis.visualize_composite_data(complete_data, begin_time=60, end_time=80,
-                             title="Dane z odnalezionymi ponownie na zakresie <65s, 75s> R-ami")
-
-print(">Próba usunięcia i odnalezienia wszystkich r")
-complete_data.delete_points('r')
-begin_time, end_time = complete_data.calculate_time_range(required_waves=['ecg'])
-found_points = analyzer.find_points(complete_data.waves, complete_data.points, begin_time, end_time, find_r, arguments)
-complete_data.add_points(found_points, 'r')
-vis.visualize_composite_data(complete_data, title="Dane z całkowicie nowymi R-ami")
-
-print(">Próba obliczenia tempa bicia serca na kilku interwałach")
-calculate_hr = analyzer.import_procedure('parameter_heart_rate')
-param_tuples = []
-for i in range(20,200,20):
-    param_tuples.append((i-20,i))
-hr = analyzer.calculate_parameter(complete_data.waves, complete_data.points, param_tuples ,calculate_hr,
-                                  calculate_hr.default_arguments)
-complete_data.add_parameter(hr, 'hr')
-vis.visualize_composite_data(complete_data, begin_time=160, end_time=240,
-                             title="Wycinek <160s; 240s> z parametrem")
-
-
-print(">Próba usunięcia pojedynczego punktu r i przesunięcia go obok")
-vis.visualize_composite_data(complete_data, begin_time=240, end_time=243, title="Wycinek <240s; 243s>")
-complete_data.points['r'].delete_point(241) #,y=5.8)
-x = 241.227
-
-y = complete_data.waves['ecg'].value_at(x)
-complete_data.points['r'].add_point(x,y)
-vis.visualize_composite_data(complete_data, begin_time=240, end_time=243, title="Wycinek <240s; 243s> z zamienionym punktem na 241s")
-
-print(">Próba oznaczenia SBP i DBP")
-find_sbp = analyzer.import_procedure('points_sbp_simple')
-arguments = find_sbp.default_arguments
-begin_time, end_time = complete_data.calculate_time_range(required_waves=['bp'])
-found_sbp = analyzer.find_points(complete_data.waves, complete_data.points, begin_time, end_time, find_sbp, arguments)
-complete_data.add_points(found_sbp, 'sbp')
-find_dbp = analyzer.import_procedure('points_dbp_simple')
-arguments = find_dbp.default_arguments
-begin_time, end_time = complete_data.calculate_time_range(required_waves=['bp'])
-found_dbp = analyzer.find_points(complete_data.waves, complete_data.points, begin_time, end_time, find_dbp, arguments)
-complete_data.add_points(found_dbp, 'dbp')
-vis.visualize_composite_data(complete_data, title="Dane z odnalezionymi SBP i DBP")
-
-print(">Próba oznaczenia DN")
-find_dn = analyzer.import_procedure('points_dn_net')
-arguments = find_dn.default_arguments
-begin_time, end_time = complete_data.calculate_time_range(required_waves=['ecg','bp'])
-found_dn = analyzer.find_points(complete_data.waves, complete_data.points, begin_time, end_time, find_dn, arguments)
-complete_data.add_points(found_dn, 'dn')
-vis.visualize_composite_data(complete_data, title="Dane z odnalezionymi DN")
+def test_modify_procedure(bp_wave, butterworth):
+    filtered_wave = analyzer.modify_wave(bp_wave, 60, 70, butterworth, 
+                                         N=3, Wn=30)
+    print(filtered_wave.value_at(5))
