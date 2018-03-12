6c4e773d504ccc634253d372a234e46e2c944371
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-03-12 20:52:51 +0100

    More tests & re-add old demo

--- test_sigman.py
+++ test_sigman.py
@@ -3,6 +3,8 @@
 # it can be run by simply using `pytest` in the `sigman-project` directory
 
 import os
+
+import numpy as np
 import pytest
 
 import sigman as sm
@@ -17,6 +19,11 @@ def bp_wave():
     return fm.import_wave('example_data/BP.dat', 'bp')
 
 @pytest.fixture
+def sine_wave():
+    sine = np.sin(np.linspace(0, 2*np.pi, num=100))
+    return sm.Wave(sine, 2, 'sine')
+
+@pytest.fixture
 def r_points():
     return fm.import_points('example_data/R.dat', 'r')
 
@@ -24,35 +31,88 @@ def r_points():
 def butterworth():
     return analyzer.import_procedure("modify_filter_butterworth")
 
+### Wave operations ###
+
 def test_import_wave_dat():
     bp_wave = fm.import_wave('example_data/BP.dat', 'bp')
     assert bp_wave.value_at(2) == 134.75
-
-def test_import_points_dat():
-    r_points = fm.import_points('example_data/R.dat', 'r')
-    assert r_points[2][0] == 2.4950000000000001
 
 def test_wave_offset(bp_wave):
     bp_wave.offset = -2
     assert bp_wave.value_at(0) == 134.75
 
+def test_wave_copy(bp_wave):
+    t_wave = bp_wave.copy()
+    assert t_wave.type == bp_wave.type
+    assert t_wave.value_at(5) == bp_wave.value_at(5)
+
+def test_wave_sample_at(bp_wave):
+    assert bp_wave.sample_at(0) == 0
+    assert bp_wave.sample_at(bp_wave.complete_length) == len(bp_wave) - 1
+    assert bp_wave.sample_at(200) == round(200*bp_wave.sample_rate)
+
+def test_value_at(bp_wave, sine_wave):
+    assert bp_wave.value_at(bp_wave.complete_length) == bp_wave[-1]
+    assert bp_wave.value_at(0) == bp_wave[0]
+    assert bp_wave.value_at(bp_wave.sample_length*200) == bp_wave[200]
+    assert sine_wave.value_at(1) == 0
+
+def test_import_points_dat():
+    r_points = fm.import_points('example_data/R.dat', 'r')
+    assert r_points[2][0] == 2.4950000000000001
+
 def test_points_offset(r_points):
     r_points.offset(-2)
     assert r_points[2][0] == 0.4950000000000001
+
+def test_composite_data_management(bp_wave, r_points):
+    composite_data = sm.Composite_data(
+        waves={
+            'bp':bp_wave},
+        points={
+            'r':r_points})
+    composite_data.delete_wave('bp')
+    composite_data.add_wave(bp_wave, 'bp')
+    with pytest.raises(ValueError):
+        composite_data.add_wave(bp_wave, 'bp')
+    composite_data.add_wave(bp_wave, 'bp', replace=True)
+    composite_data.delete_points('r')
+    composite_data.add_points(r_points, 'r')
+    with pytest.raises(ValueError):
+        composite_data.add_points(r_points, 'r')
+    slice_ = composite_data.points['r'].data_slice(20, 30)
+    slice_points = sm.Points(slice_[0], slice_[1], 'r')
+    assert slice_points[0][0] == 20.618680000000001
+    assert composite_data.points['r'][22][0] == 20.618680000000001
+    composite_data.points['r'].delete_slice(20, 30)
+    assert composite_data.points['r'][22][0] != 20.618680000000001
+    composite_data.add_points(slice_points, 'r', join=True)
+    assert composite_data.points['r'][22][0] == 20.618680000000001
 
 def test_procedure_import():
     butterworth = analyzer.import_procedure("modify_filter_butterworth")
     assert butterworth.author == 'kcybulski'
 
+### FIXME: Should be removed after issue #20
 def test_deprecated_modify_procedure(bp_wave, butterworth):
     arguments = butterworth.default_arguments
     arguments['N'] = 3
     arguments['Wn'] = 30
     filtered_wave = analyzer.modify_wave(bp_wave, 60, 70, butterworth,
                                          arguments)
-    assert 90.118839599334365 == filtered_wave.value_at(5)
+    assert filtered_wave.value_at(5) == 90.118839599334365
+    assert filtered_wave.complete_length == 10
+    assert bp_wave.value_at(65) == 89.75
+    bp_wave.replace_slice(60, 70, filtered_wave)
+    assert bp_wave.value_at(65) == 90.118839599334365
 
+### FIXME: Will pass after issue #20
 def test_modify_procedure(bp_wave, butterworth):
     filtered_wave = analyzer.modify_wave(bp_wave, 60, 70, butterworth, 
                                          N=3, Wn=30)
-    print(filtered_wave.value_at(5))
+    assert 90.118839599334365 == filtered_wave.value_at(5)
+    assert filtered_wave.value_at(5) == 90.118839599334365
+    assert filtered_wave.complete_length == 10
+    assert bp_wave.value_at(65) == 89.75
+    bp_wave.replace_slice(60, 70, filtered_wave)
+    assert bp_wave.value_at(65) == 90.118839599334365
