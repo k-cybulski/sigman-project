46d6512bef3c00775001a4e17318fac7ac340002
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-03-31 16:48:52 +0200

    Translage file_manager

--- sigman/file_manager.py
+++ sigman/file_manager.py
@@ -1,9 +1,6 @@
 """
-W tym pliku zawarte są funkcje służące do zapisywania i wczytywania
-danych do analizy.
+This file contains functions allowing the import and export of data.
 """
-# TODO: save_composite_data oraz open_composie_data nie powinny korzystać z pickle; jest wolny, może kreować masywne pliki przy wielkich ilościach danych oraz jest niebezpieczny - pozwala na uruchamianie kodu bez wiedzy podczas włączania
-
 
 import csv
 import os.path
@@ -14,25 +11,25 @@ import numpy as np
 import sigman as sm 
 
 def save_composite_data(file_name, composite_data):
-    """Zapisuje dany Composite_data w pliku .pickle."""
+    """Saves the given `Composite_data` in a pickle file."""
     with open(file_name, 'wb') as pickle_file:
         pickle.dump(composite_data, pickle_file)
 
 def load_composite_data(file_name):
-    """Wczytuje zapisany w .pickle Composite_data."""
+    """Loads `Composite_data` from a given pickle file."""
     with open(file_name, 'rb') as pickle_file:
         return pickle.load(pickle_file)
 
 def _import_dat(file_name):
-    """Importuje dwie tablice współrzędnych z pliku .dat."""
+    """Imports two tables of coordinates from a .dat file."""
     x = []
     y = []
     with open(file_name) as csv_file:
         reader = csv.reader(csv_file, delimiter=' ')
         for row in reader:
             x.append(float(row[0]))
-            # Niektóre pliki .dat mają dwie spacje zamiast jednej.
-            # W takim wypadku to row[1] będzie pusty
+            # Some .dat files have two spaces instead of one, in which case
+            # row[1] is empty
             if row[1]=="": 
                 y.append(float(row[2]))
             else:
@@ -41,9 +38,8 @@ def _import_dat(file_name):
 
 
 def _import_wave_dat(file_name, wave_type, offset=0):
-    """Importuje przebieg o stałej częstotliwości z pliku .dat i
-    zwraca odpowiadający mu sm.Wave.
-    """
+    """Imports a waveform of constant frequency from a .dat file and
+    returns a corresponding `Wave`."""
     x, y = _import_dat(file_name)
     complete_len = x[-1]
     return sm.Wave(y, complete_len, 
@@ -51,76 +47,71 @@ def _import_wave_dat(file_name, wave_type, offset=0):
                         offset = offset)
     
 def _import_point_dat(file_name, point_type):
-    """Importuje współrzędne punktów z pliku .dat i zwraca odpowiadający
-    im sm.Points.
-    """
+    """Imports coordinates from a .dat file and returns a corresponding
+    `Points` instance."""
     x, y = _import_dat(file_name)    
     return sm.Points(x, y, 
                           point_type = point_type)
 
 def import_wave(file_name, wave_type, offset=0):
-    """Importuje przebieg z danego pliku, przy czym wybiera odpowiednią
-    funkcję do formatu danego pliku.
-    """
+    """Imports a `Wave` instance from a given file."""
     extension = os.path.splitext(file_name)[1][1:]
     if extension == 'dat':
         import_func = _import_wave_dat
     else:
-        raise ValueError("Nieodpowiedni format plików")
+        raise ValueError("Invalid file format")
     return import_func(
         file_name, 
         wave_type = wave_type, 
         offset = offset)
 
 def import_points(file_name, point_type):
-    """Importuje punkty z danego pliku, przy czym wybiera odpowiednią
-    funkcję do formatu danego pliku.
-    """
+    """Imports a `Points` instance from a given file."""
     extension = os.path.splitext(file_name)[1][1:]
     if extension == 'dat':
         import_func = _import_point_dat
     else:
-        raise ValueError("Nieodpowiedni format plików")
+        raise ValueError("Invalid file format")
     return import_func(
         file_name,
         point_type = point_type)
 
 def _export_dat(data_x,data_y,filename):
-    """Zapisuje dwie tablice współrzędnych w pliku .dat."""
+    """Writes two coordinate tables in a .dat file"""
     with open(filename, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=' ')
         for x, y in zip(data_x, data_y):
             writer.writerow([x,y])
 
 def _export_line_dat(file_name, wave):
-    """Eksportuje Wave do pliku o formacie .dat."""
+    """Exports `Wave` to a .dat file."""
     data_x, data_y = wave.generate_coordinate_tables()
     _export_dat(data_x,data_y,file_name)
 
 def _export_point_dat(file_name, points):
-    """Eksportuje Points do pliku o formacie .dat."""
+    """Exports `Points` to a .dat file."""
     _export_dat(file_name, points.data_x, points.data_y)
 
 def export_line(file_name, wave):
-    """Eksportuje Wave do pliku, wykorzystując przy tym funkcję
-    odpowiednią dla pożądanego formatu.
+    """Exports `Wave` into a file with the format depending on
+    the extension.
     """
     extension = os.path.splitext(file_name)[1][1:]
     if extension == 'dat':
         export_func = _export_line_dat
     else:
-        raise ValueError("Nieodpowiedni format plików")
+        raise ValueError("Invalid file format")
     export_func(file_name, wave)
 
 def export_points(file_name, points):
-    """Eksportuje Points do pliku, wykorzystując przy tym funkcję
-    odpowiednią dla pożądanego formatu.
+    """Exports `Points` into a file with the format depending on
+    the extension.
     """
     extension = os.path.splitext(file_name)[1][1:]
     if extension == 'dat':
         export_func = _export_point_dat
     else:
-        raise ValueError("Nieodpowiedni format plików")
+        raise ValueError("Invalid file format")
     export_func(file_name, points)
 
 def _estimate_points_offset(align_points, reference_points,
